8e7964193ff974ed05988dee1ed32fbe2b1b2829
imdamannow711-ui <im.da.man.now.711@gmail.com>
2026-08-13 06:34:22 -0400

    Refresh Creator OS voiceover cache

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    connection. That means a bad upload can never get stuck on the phone
    the way it did before. */
 
-const CACHE_NAME = "done-rite-v10-chatgpt-voiceover-share";
+const CACHE_NAME = "done-rite-v11-reusable-voiceover-library";
 
 const FILES = ["./", "./index.html", "./app.js", "./teleprompter.html", "./video-upload.html", "./content-gap-import.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
